74a7fe73ab674f49ee2dedb2f1a4ad6f900cf949
ledza228 <leshyash2003@gmail.com>
2023-08-18 21:58:15 +0300

    task10

--- src/main/java/ru/yandex/tasks/Task10Permutations.java
+++ src/main/java/ru/yandex/tasks/Task10Permutations.java
@@ -1,14 +1,42 @@
 package ru.yandex.tasks;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Task10Permutations {
+
+    public static void makePermutation(int[] numbers, int startIndex, List<int[]> res){
+        if (startIndex == numbers.length - 1){
+            res.add(numbers);
+            return;
+        }
+
+        for (int i=startIndex; i<numbers.length; i++){
+            int[] newArray = Arrays.copyOf(numbers, numbers.length);
+            int temp = numbers[i];
+            numbers[i] = numbers[startIndex];
+            numbers[startIndex] = temp;
+            makePermutation(newArray, startIndex+1, res);
+        }
+
+    }
+
     public static int[][] permutations(int[] numbers) {
         /*
          * Возвращает массив со всеми перестановками массива numbers (массив n чисел от 1 до n, 1 <= n <= 10)
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+
+        List<int[]> list = new ArrayList<>();
+        makePermutation(numbers, 0, list);
+
+        int[][] res = new int[list.size()][];
+
+        for (int i=0;i<res.length;i++){
+            res[i] = list.get(i);
+        }
+        return res;
     }
 
     public static void selfCheck() {
@@ -23,5 +51,10 @@ public class Task10Permutations {
         };
 
         assert Arrays.deepEquals(output, permutations(input));
+    }
+
+    public static void main(String[] args) {
+        selfCheck();
+///        permutations(new int[]{1, 2, 3});
     }
 }
