9935a4ec16fd5570f1679850643931a92d0f5af5
ledza228 <leshyash2003@gmail.com>
2023-08-18 20:28:43 +0300

    task 7

--- src/main/java/ru/yandex/tasks/Task7PrimeNumbers.java
+++ src/main/java/ru/yandex/tasks/Task7PrimeNumbers.java
@@ -2,7 +2,9 @@
 
 package ru.yandex.tasks;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Task7PrimeNumbers {
     public static int[] findPrimes (int N) {
@@ -11,7 +13,26 @@ public class Task7PrimeNumbers {
          * Выход: отсортированный массив всех простых чисел от 2 до N
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+
+        boolean[] resheto = new boolean[N+1];
+
+        for (int i=2;i<Math.sqrt(resheto.length);i++){
+            for (int j=i*i;j<N+1; j++){
+            if (j % i == 0){
+                resheto[j] = true;
+            }
+        }
+        }
+
+        List<Integer> res = new ArrayList<>();
+
+        for (int i=2;i<resheto.length;i++){
+            if (!resheto[i]){
+                res.add(i);
+            }
+        }
+
+        return res.stream().mapToInt(i -> i).toArray();
     }
 
     public static void selfCheck() {
